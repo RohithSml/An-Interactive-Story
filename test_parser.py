ebdb7bd1dfdadbe9c1397f2692abd6fca1b5160e
Rohith Samuel <rohith.sml94@gmail.com>
2019-03-03 17:07:20 +0530

    RED: Test for Initializing State

--- test_parser.py
+++ test_parser.py
@@ -7,3 +7,25 @@ def test_loading_map():
     assert status == True
 
     
+def test_initial_state():
+    map_data,status = load_map("./test_data/test_map.json")
+    Init_state(map_data)
+    estimate={ 'items': [{'Room 0':'', 'Creeky Hallway':'','Dusty Hallway':'','Laboratory':'','Library':'','Secret Room':'','End':'','Coal Chute':'', 'Organ Room':'battered_ring', 'Chapel':'lantern', 'Conservatory':'skull', 'Bloody Room':'sacrificial_dagger'}],
+
+        'Starting_Room': 'Room 0',
+
+        'Current_Room' : 'Room 0',
+                
+        'inventory' : [],
+
+        'passive_item' : ['lantern', 'Dusty Hallway'],
+        
+        'secret_room_status' : [False, False],
+
+        'secret_room_avl' :{'Organ Room':{'north' : 'Sceret Room','east':'Creeky Hallway'}},
+
+        'End' :'To be continued'
+
+        }
+
+    assert state==estimate
